d770ceef73a3a28764c9c610b64ff022153661b3
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2024-02-09 22:52:23 +0530

    Time: 3 ms (73.73%) | Memory: 17.2 MB (35.09%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -11,47 +11,28 @@
  */
 class Solution {
 public:
-    void Inorder(TreeNode* root,vector<int>&v)
-    {    
-        if(root==NULL)
+     void Inorder(TreeNode* p, TreeNode* q, int &flag)
+    {
+       
+        if((p!=NULL and q==NULL) or (q!=NULL and p==NULL))
         {
+            flag = 1;
             return;
         }
-        Inorder(root->left,v);
-        if(root!=NULL and root->right!=NULL and root->left==NULL)
+        if(p==NULL or q==NULL) return;
+        Inorder(p->left,q->right,flag);
+        if(p->val!=q->val)
         {
-            v.push_back(-120);
+            flag=1;
+            return;
         }
-         v.push_back(root->val);
-         if(root!=NULL and root->right==NULL and root->left!=NULL)
-        {
-            v.push_back(-120);
-        }
-        Inorder(root->right,v);
+        Inorder(p->right,q->left,flag);
     }
     bool isSymmetric(TreeNode* root) {
-        vector<int>v;
-        Inorder(root,v);
         if(root==NULL) return true;
-        if(root!=NULL and root->left!=NULL and root->right!=NULL)
-        {
-            if(root->right->val!=root->left->val)
-            {
-                return false;
-            }
-        }
-        int i = 0;
-        int j = v.size()-1;
-        // for(int i = 0 ; i<=v.size()-1;i++)
-        // {
-        //     cout<<v[i]<<" ";
-        // }
-        while(i<=j)
-        {
-            if(v[i]!=v[j]) return false;
-            i+=1;
-            j-=1;
-        }
-        return true;
+        int flag=0;
+        Inorder(root->left,root->right,flag);
+        if(flag==0) return true;
+        return false;
     }
 };
