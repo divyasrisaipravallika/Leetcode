91b94e6da952c7c2b9c7c905ef16c312cbf8ed5a
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2024-02-18 22:18:18 +0530

    Time: 85 ms (34.03%) | Memory: 57.6 MB (21.99%) - LeetSync

---
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& h, int b, int l) {
+        priority_queue<int>q;
+        int c = 0;
+        int f = 0,i=0;
+        for(i = 0; i < h.size()-1; i++){
+            if(h[i]>=h[i+1]) {
+                c++;
+            }
+            else{
+              int d = abs(h[i]-h[i+1]);
+              b-=d;
+              q.push(d);
+              if(b>=0) c++;
+              else if(b<0 and l>0){
+
+                  b+=q.top();
+                  q.pop();
+                  l-=1;
+              }  
+              else if(b<0 and l<=0){
+                  return i;
+              }
+            }
+          }
+        return i;
+    }
+};
